610eec4869eb1ed2cfc49fe8ee3189b6f51dd36f
fabio.maiorano <fabio.maiorano@D-CB-001330>
2018-05-28 06:55:46 -0300

    add: call github api for list repositories

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -4,19 +4,30 @@ import api from '../../services/api';
 
 class Main extends Component {
   state = {
-    repositorySearch: '',
+    repositorySearch: 'facebook/react',
+    repositories: [],
   };
 
   componentDidMount() {}
 
-  handleAddRepository = async () => {};
+  handleAddRepository = async (e) => {
+    e.preventDefault();
+    const { data } = await api.get(`/repos/${this.state.repositorySearch}`);
+    const repository = {
+      name: data.name,
+      id: data.id,
+      organization: data.organization,
+    };
+    this.setState({ repositories: [...this.state.repositories, repository] });
+  };
 
   render() {
     return (
-      <form onSubmit="handleAddRepository">
+      <form onSubmit={this.handleAddRepository}>
         <input
           type="text"
           placeholder="usuário/repositório"
+          value={this.state.repositorySearch}
           onChange={e => this.setState({ repositorySearch: e.target.value })}
         />
         <button type="submit">Adicionar</button>
