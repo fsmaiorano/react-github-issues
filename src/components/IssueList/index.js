a034787cb059dfc124102a0e8b27e5859a498d5d
fabio.maiorano <fabio.maiorano@D-CB-001330>
2018-05-29 14:36:05 -0300

    update: some changes on the structure

--- src/components/IssueList/index.js
+++ src/components/IssueList/index.js
@@ -1,5 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const IssueList = ({ repository }) => <div>{repository.name}</div>;
+import { Container } from './styles';
+
+const IssueList = ({ repository }) => (
+  <Container>
+    {repository.issues &&
+      repository.issues.map(issue => (
+        <div key={issue.id}>
+          <img src={issue.user.avatar_url} alt={issue.title} />
+          <p>{issue.title}</p>
+          <p>{issue.user.login}</p>
+        </div>
+      ))}
+  </Container>
+);
+
+IssueList.prototype = {
+  repository: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    user: PropTypes.shape({
+      login: PropTypes.string,
+      avatar_url: PropTypes.string,
+    }),
+    getIssues: PropTypes.func,
+  }).isRequired,
+};
 
 export default IssueList;
--- src/components/IssueList/styles.js
+++ src/components/IssueList/styles.js
@@ -1,1 +1,9 @@
 import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  padding: 10px 1px;
+  cursor: pointer;
+`;
